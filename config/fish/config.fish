f828c65109eee4425c7b07b7fbc97fe542569c79
aqyuki <yksk171118@gmail.com>
2025-04-07 06:40:48 +0900

    feat(fish): fzfとのshell integrationを有効化

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,10 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -g XDG_CONFIG_HOME $HOME/.config
-set -g XDG_CACHE_HOME $HOME/.cache
-set -g XDG_DATA_HOME $HOME/.local/share
-set -g XDG_STATE_HOME $HOME/.local/state
+set -Ux XDG_CONFIG_HOME $HOME/.config
+set -Ux XDG_CACHE_HOME $HOME/.cache
+set -Ux XDG_DATA_HOME $HOME/.local/share
+set -Ux XDG_STATE_HOME $HOME/.local/state
 
 # setup go command
 fish_add_path /usr/local/go/bin
@@ -16,6 +16,14 @@ abbr -a lg lazygit
 
 # activate mise
 mise activate fish | source
+
+# GHQ settings
+set -Ux GHQ_ROOT $HOME/projects
+
+# activate fzf integration
+fzf --fish | source
+set -Ux FZF_DEFAULT_OPTS '--height 50% --layout reverse --border rounded'
+bind \cg __fzf_project_directory
 
 # load prompt
 starship init fish | source
---
+++ config/fish/functions/__fzf_project_directory.fish
@@ -0,0 +1,7 @@
+function __fzf_project_directory
+    set src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+    if test -n "$src"
+        cd (ghq root)/$src
+        commandline -f repaint
+    end
+end
