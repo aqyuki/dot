1833faaf63804a8358338b74b85c91dbc4e645e4
aqyuki <yksk171118@gmail.com>
2025-04-08 17:45:10 +0900

    chore(nvim): 関数のdescriptionを追加

--- config/fish/functions/__fzf_project_directory.fish
+++ config/fish/functions/__fzf_project_directory.fish
@@ -1,4 +1,4 @@
-function __fzf_project_directory
+function __fzf_project_directory --description "find local repository with fzf"
     set src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
     if test -n "$src"
         cd (ghq root)/$src
