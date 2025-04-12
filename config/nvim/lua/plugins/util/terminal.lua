3c514d304087e36b37de3f91410b5a3e3bfc68bd
aqyuki <yksk171118@gmail.com>
2025-04-12 20:52:39 +0900

    feat(nvim): which-keyの設定を追加

--- config/nvim/lua/plugins/util/terminal.lua
+++ config/nvim/lua/plugins/util/terminal.lua
@@ -1,16 +1,29 @@
 return {
-  "akinsho/toggleterm.nvim",
-  lazy = true,
-  event = "VeryLazy",
-  keys = {
-    { mode = "n", "<localleader>tn", ":TermNew<CR>", desc = "Create new terminal" },
-    { mode = "n", "<localleader>ts", ":TermSelect<CR>", desc = "Select new terminal" },
+  {
+    "akinsho/toggleterm.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    keys = {
+      { mode = "n", "<localleader>tn", ":TermNew<CR>", desc = "Create new terminal" },
+      { mode = "n", "<localleader>ts", ":TermSelect<CR>", desc = "Select new terminal" },
+    },
+    opts = {
+      open_mapping = "<localleader>tt",
+      direction = "float",
+      on_open = function(t)
+        vim.keymap.set("t", "<Space>", "<Space>", { buffer = t.bufnr })
+      end,
+    },
   },
-  opts = {
-    open_mapping = "<localleader>tt",
-    direction = "float",
-    on_open = function(t)
-      vim.keymap.set("t", "<Space>", "<Space>", { buffer = t.bufnr })
-    end,
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        {
+          mode = "n",
+          { "<localleader>t", group = "Terminal" },
+        },
+      },
+    },
   },
 }
