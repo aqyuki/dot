4853083e65f6ca87deeab26228ea7dabd0fa400b
aqyuki <yksk171118@gmail.com>
2025-04-17 20:51:29 +0900

    feat(nvim): change default formatter in Go

--- config/nvim/lua/plugins/lang/go.lua
+++ config/nvim/lua/plugins/lang/go.lua
@@ -1,13 +1,25 @@
 return {
   {
     "stevearc/conform.nvim",
-    go = { "goimports", "gci" },
+    opts = {
+      formatters_by_ft = {
+        go = { "gci" },
+      },
+      formatters = {
+        gci = {
+          command = "gci",
+          args = { "write", "$FILENAME" },
+          stdin = false,
+          inhert = false,
+        },
+      },
+    },
   },
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "goimports", "gci" })
+      vim.list_extend(opts.ensure_installed, { "gci" })
     end,
   },
 }
